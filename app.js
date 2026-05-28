eaeb8b8778706515d36d8df2128631c5d93a7aca
BOOBESH776 <cboobesh776@gmail.com>
2026-05-28 23:14:03 +0530

    remove some tag space and remove useless variable

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("data")) || [];
 let CompleteTask = 0;
-
 function btn() {
     Task();
 }
@@ -49,65 +48,62 @@ function Task() {
     `);
 
     tasks.push(UserTask);
+    SavaData();
+        // TASK COUNT
+        const yt = document.getElementById("yt").textContent = tasks.length;
+        input.value = "";
+    }
 
-    // TASK COUNT
-    const yt = document.getElementById("yt").textContent = tasks.length;
-    input.value = "";
-}
+    const Task_Card = document.getElementById("Task_Card");
+    Task_Card.addEventListener("change", function (e) {
 
-const Task_Card = document.getElementById("Task_Card");
-Task_Card.addEventListener("change", function (e) {
-    const ct = document.getElementById("yt");
+        if (e.target.classList.contains("check")) {
+            let taskText = e.target.nextElementSibling;
 
-    if (e.target.classList.contains("check")) {
-        let taskText = e.target.nextElementSibling;
+            if (e.target.checked) {
+                taskText.style.textDecoration = "line-through";
+                taskText.style.color = "green";
+                CompleteTask++;
 
-        if (e.target.checked) {
-            taskText.style.textDecoration = "line-through";
-            taskText.style.color = "green";
-            CompleteTask++;
+            } else {
 
-        } else {
+                taskText.style.textDecoration = "none";
+                taskText.style.color = "black";
+                CompleteTask--;
+            }
+            let ct = document.getElementById("ct").textContent = CompleteTask;;
 
-            taskText.style.textDecoration = "none";
-            taskText.style.color = "black";
-            CompleteTask--;
         }
-        let ct = document.getElementById("ct").textContent = CompleteTask;;
-
-    }
-});
+    });
 
 
-Task_Card.addEventListener("click", function (e) {
+    Task_Card.addEventListener("click", function (e) {
 
-    // DELETE TASK
+        // DELETE TASK
 
-    if (e.target.classList.contains("delete")) {
+        if (e.target.classList.contains("delete")) {
 
-        e.target.closest(".card-body").remove();
+            e.target.closest(".card-body").remove();
 
-        tasks.pop();
-
-        const yt = document.getElementById("yt");
-
-        document.getElementById("yt").textContent = tasks.length;
-    }
+            tasks.pop();
+            SavaData()
+            document.getElementById("yt").textContent = tasks.length;
+        }
 
 
-    // EDIT TASK
+        // EDIT TASK
 
-    if (e.target.classList.contains("edit")) {
+        if (e.target.classList.contains("edit")) {
 
-        const cardBody = e.target.closest(".card-body");
+            const cardBody = e.target.closest(".card-body");
 
-        const taskText = cardBody.querySelector(".task-text");
+            const taskText = cardBody.querySelector(".task-text");
 
-        const currentText = taskText.innerText;
+            const currentText = taskText.innerText;
 
-        taskText.outerHTML = `
+            taskText.outerHTML = `
 
-                    < span class="edit-area d-flex gap-2" >
+                    <span class="edit-area d-flex gap-2">
 
                         <input
                             type="text"
@@ -123,49 +119,53 @@ Task_Card.addEventListener("click", function (e) {
                                 ✖
                             </button>
 
-                        </>
+                        </span>
 
             `;
-    }
-
-
-    // SAVE BTN
-
-    if (e.target.classList.contains("save-btn")) {
-
-        const cardBody = e.target.closest(".card-body");
-
-        const inputField = cardBody.querySelector(".edit-input");
-
-        const newValue = inputField.value.trim();
-
-        if (newValue === "") {
-            alert("Task cannot be empty");
-
         }
 
-        cardBody.querySelector(".edit-area").outerHTML = `
 
-                < span class="task-text" >
+        // SAVE BTN
+
+        if (e.target.classList.contains("save-btn")) {
+
+            const cardBody = e.target.closest(".card-body");
+
+            const inputField = cardBody.querySelector(".edit-input");
+
+            const newValue = inputField.value.trim();
+
+            if (newValue === "") {
+                alert("Task cannot be empty");
+                return;
+            }
+
+            cardBody.querySelector(".edit-area").outerHTML = `
+
+                <span class="task-text">
                     ${newValue}
-            </ >
+            </span>
 
                 `;
-    }
+            SavaData()
+        }
 
 
-    // CANCEL BTN
+        // CANCEL BTN
 
-    if (e.target.classList.contains("cancel-btn")) {
-        const cardBody = e.target.closest(".card-body");
-        const inputField = cardBody.querySelector(".edit-input");
-        const oldValue = inputField.defaultValue;
-        cardBody.querySelector(".edit-area").outerHTML = `
-                < span class="task-text" >
+        if (e.target.classList.contains("cancel-btn")) {
+            const cardBody = e.target.closest(".card-body");
+            const inputField = cardBody.querySelector(".edit-input");
+            const oldValue = inputField.defaultValue;
+            cardBody.querySelector(".edit-area").outerHTML = `
+                <span class="task-text">
                     ${oldValue}
-            </ >
+            </span>
 
                 `;
-    }
+        }
 
-});
+    });
+    function SavaData() {
+        localStorage.setItem("data", JSON.stringify(tasks));
+    }
